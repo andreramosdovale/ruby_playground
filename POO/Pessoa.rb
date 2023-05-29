a861fcf0ad49b826bff0702898336bba53f87933
André Ramos <andre_ramosdovale@outlook.com>
2023-05-28 21:56:16 -0300

    know more opp in ruby

--- POO/Pessoa.rb
+++ POO/Pessoa.rb
@@ -1,13 +1,25 @@
 class Pessoa
-    def initialize()
-        puts "inicializando"
-    end
-
-    def falarNome(nome = "John Doe")
-        "Olá, #{nome}"
-    end
+    attr_accessor :name
 end
 
 p1 = Pessoa.new
 
-puts p1.falarNome("André")
+p1.name = 'André'
+puts p1.name
+
+puts '==============='
+
+class Pessoa2
+    def initialize(nome_fornecido = 'indigente')
+        @nome = nome_fornecido
+    end
+
+    def imprimirNome
+        @nome
+    end
+end
+    
+p2 = Pessoa2.new("André 2")
+puts p2.imprimirNome
+
+puts '==============='
