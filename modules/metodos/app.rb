9b41a971e0fd4152eabaa183feb8ee84a345096a
André Ramos <andre_ramosdovale@outlook.com>
2023-06-05 22:56:08 -0300

    modulos

---
+++ modules/metodos/app.rb
@@ -0,0 +1,4 @@
+require_relative "pagamento"
+include Pagamento
+
+puts pagar('b','n','v')
---
+++ modules/metodos/pagamento.rb
@@ -0,0 +1,7 @@
+module Pagamento
+    PI = 3.14
+
+    def pagar(bandeira, numero, valor)
+        "pagando o cartão #{bandeira} numero #{numero}, o valor de #{valor}"
+    end
+end
